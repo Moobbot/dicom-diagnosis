21a20842b12b8c9eb112bc4c9962e334062d1e38
NgoTam <ngotam24082001@gmail.com>
2025-03-16 16:45:27 +0700

    Refactor authentication API responses to use standard Response object and improve JSON handling

--- frontend/app/api/auth/route.ts
+++ frontend/app/api/auth/route.ts
@@ -1,23 +1,26 @@
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
     const body = await request.json();
     const { refreshToken } = body;
     if (!refreshToken) {
-        return NextResponse.json(
-            { message: 'Refresh token not received' },
-            { status: 400 }
-        );
+        return new Response(
+            JSON.stringify({ message: 'Refresh token not received' }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
     }
 
-    return NextResponse.json(body, {
+    return new Response(JSON.stringify(body), {
         status: 200,
         headers: {
+            'Content-Type': 'application/json',
             'Set-Cookie': `refreshToken=${refreshToken}; Path=/; HttpOnly; SameSite=Strict;`
         }
     });
-
 
 }
 
@@ -25,19 +28,18 @@ export async function GET(request: Request) {
     const cookieStore = await cookies();
     const refreshToken = cookieStore.get('refreshToken')?.value;
 
-    return NextResponse.json(refreshToken, {
-        status: 200,
+    return new Response(JSON.stringify({ refreshToken }), {
+        status: 200
     });
 }
 
 export async function DELETE(request: Request) {
-    return NextResponse.json(
-        { message: 'Access token and refresh token deleted' },
+    return new Response(
+        JSON.stringify({ message: 'Access token and refresh token deleted' }),
         {
             status: 200,
             headers: {
                 'Set-Cookie': 'refreshToken=; Path=/; HttpOnly; SameSite=Strict; Max-Age=0'
             }
-        }
-    );
+        });
 }
